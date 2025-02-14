c424fed00cb22aa347fdc2d9f60d23a02339075b
unknown <olivierhumanbooster@gmail.com>
2025-02-14 16:31:59 +0100

    ajout dossier back et front

---
+++ manga-up-back/src/main/java/com/mangaup/manga_up/MangaUpApplication.java
@@ -0,0 +1,13 @@
+package com.mangaup.manga_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaUpApplication.class, args);
+	}
+
+}
---
+++ manga-up-back/src/test/java/com/mangaup/manga_up/MangaUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangaup.manga_up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
