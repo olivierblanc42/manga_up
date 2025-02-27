9bc23125bc647f359dd57a8cf6b9f9c7288d654d
unknown <olivierhumanbooster@gmail.com>
2025-02-27 15:52:10 +0100

    ajout de swipper

--- manga-up-front/src/app/pages/home/home.component.ts
+++ manga-up-front/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
 
