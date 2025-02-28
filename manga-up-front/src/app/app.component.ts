a7569014f32b7f625a56c70f1060a7075c30280d
unknown <olivierhumanbooster@gmail.com>
2025-02-28 16:32:18 +0100

    off canvas

--- manga-up-front/src/app/app.component.ts
+++ manga-up-front/src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'manga-up-front';
+  sidenav = viewChild<ElementRef<HTMLElement>>('mySidenav');
+  ngOnInit(): void {
+
+  }
+
+   openNav() {
+    const element = this.sidenav();
+    if (element) {
+      element.nativeElement.style.width = '100%';
+    }
+  }
+  
+  closeNav() {
+    const element = this.sidenav();
+    if (element) {
+      element.nativeElement.style.width = '0';
+    }
+  }
+
 }
+
+
--- manga-up-front/src/app/pages/home/home.component.ts
+++ manga-up-front/src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  schemas : [CUSTOM_ELEMENTS_SCHEMA]
+  schemas : [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit{
+
+
+  ngOnInit(): void {
+ 
+
+  }
+
 
 }
