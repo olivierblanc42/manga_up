8d65b2a3e902e2839414d2de8c4fe034b843fb72
unknown <olivierhumanbooster@gmail.com>
2025-03-06 19:39:53 +0100

    page manga page genre

--- manga-up-front/src/app/app.routes.ts
+++ manga-up-front/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { MangasComponent } from './pages/mangas/mangas.component';
 import { MangaComponent } from './pages/manga/manga.component';
 
 import { GenresComponent } from './pages/genres/genres.component';
+import { GenreComponent } from './pages/genre/genre.component';
+
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { AuteursComponent } from './pages/auteurs/auteurs.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
@@ -11,6 +13,7 @@ export const routes: Routes = [
     { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
     { path: 'mangas', component: MangasComponent },
     { path: 'genres', component: GenresComponent },
+    { path: 'genre', component: GenreComponent },
     { path: 'categories', component: CategoriesComponent },
     { path: 'auteurs', component: AuteursComponent },
     { path: 'privacy', component: PrivacyPolicyComponent },
