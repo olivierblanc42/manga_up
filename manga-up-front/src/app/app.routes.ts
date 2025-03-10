574aa85ade03b461886672e31b2aa8361d64471b
unknown <olivierhumanbooster@gmail.com>
2025-03-10 09:55:49 +0100

    404 et fin page genre

--- manga-up-front/src/app/app.routes.ts
+++ manga-up-front/src/app/app.routes.ts
@@ -7,16 +7,28 @@ import { GenresComponent } from './pages/genres/genres.component';
 import { GenreComponent } from './pages/genre/genre.component';
 
 import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategorieComponent } from './pages/categorie/categorie.component';
+
+
 import { AuteursComponent } from './pages/auteurs/auteurs.component';
+import { AuteurComponent } from './pages/auteur/auteur.component';
+
+
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 export const routes: Routes = [
     { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
     { path: 'mangas', component: MangasComponent },
     { path: 'genres', component: GenresComponent },
     { path: 'genre', component: GenreComponent },
     { path: 'categories', component: CategoriesComponent },
+    { path: 'categorie', component: CategorieComponent },
+
     { path: 'auteurs', component: AuteursComponent },
+    { path: 'auteur', component: AuteurComponent },
+
     { path: 'privacy', component: PrivacyPolicyComponent },
     { path: 'manga', component: MangaComponent },
-
+    { path: '404', component: NotFoundComponent },
+    { path: '**', redirectTo: '/404' } 
 ];
