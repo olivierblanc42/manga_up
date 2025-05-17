dac6e12a0933e3409a7d0fe0a3c2a13262964d5e
unknown <olivierhumanbooster@gmail.com>
2025-05-17 08:28:31 +0200

    creation du service search et ajout des methodes

---
+++ manga-up-front/src/app/service/search.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, lastValueFrom, Observable } from 'rxjs';
+import { MangaBaseProjection, MangaBaseProjections, UserTest } from '../type';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SearchService{
+    urlSearch= "api/public/search";
+
+    options = {
+        headers: new HttpHeaders({
+            "Content-Type": "application/json",
+        }),
+        body: {
+            id: '',
+        },
+    };
+    constructor(
+        private http: HttpClient
+    ) { }
+    mangasSearch = new BehaviorSubject<MangaBaseProjections| null>(null)
+    currentmangasSearch = this.mangasSearch.asObservable();
+
+
+
+    async getMangas(letter: string, page: number = 0) {
+        try {
+            const url = `${this.urlSearch}/${letter}?page=${page}`;
+            const r = await lastValueFrom(this.http.get<MangaBaseProjections>(url));
+            if (!r) return;
+            this.mangasSearch.next(r);
+            console.log(r)
+        } catch (err) {
+            console.error('Erreur lors de la recherche paginée :', err);
+        }
+      }
+
+
+
+} 
