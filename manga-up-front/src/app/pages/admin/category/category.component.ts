cabf98ab330abbf21d1f91b8682f495e59052274
unknown <olivierhumanbooster@gmail.com>
2025-05-20 08:19:59 +0200

    gestion pour la mise a jour des données

--- manga-up-front/src/app/pages/admin/category/category.component.ts
+++ manga-up-front/src/app/pages/admin/category/category.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryWithMangas } from '../../../type';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CategoryService } from '../../../service/category.service';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-category',
-  imports: [],
+  selector: 'app-category-admin',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.scss'
+  styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent {
+export class CategoryAdminComponent implements OnInit {
 
+  idOfUrl!: number;
+  category: CategoryWithMangas | null = null;
+  categoryForm!: FormGroup;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private categoryService: CategoryService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.idOfUrl = parseInt(id);
+      if (!isNaN(this.idOfUrl)) {
+        this.loadCategory(this.idOfUrl);
+      }
+    }
+
+    this.categoryService.currentcategoriesWithManga.subscribe(data => {
+      this.category = data;
+      if (this.category) {
+        // On met à jour le formulaire avec les données existantes
+        this.categoryForm.patchValue({
+          label: this.category.label,
+          description: this.category.description,
+          url: this.category.url
+        });
+      }
+      console.log('category', this.category);
+    });
+  }
+
+  initForm(): void {
+    this.categoryForm = this.fb.group({
+      label: ['', Validators.required],
+      description: ['', Validators.required],
+      url: ['', Validators.required]
+    });
+  }
+
+  loadCategory(id: number): void {
+    this.categoryService.getCategory(id);
+  }
+
+  async onSubmit() {
+    if (this.categoryForm.valid) {
+      const updatedCategory = {
+        id: this.idOfUrl,
+        ...this.categoryForm.value
+      };
+
+      try {
+        console.log(updatedCategory);
+
+
+
+        await this.categoryService.updateCategory(updatedCategory);
+        alert('Catégorie mise à jour avec succès !');
+        this.router.navigate(['/admin/categoriesAdmin']); // ou une autre route de retour
+      } catch (error) {
+        console.error('Erreur lors de la mise à jour', error);
+        alert('Erreur lors de la mise à jour');
+      }
+    } else {
+      this.categoryForm.markAllAsTouched();
+    }
+  }
 }
--- manga-up-front/src/app/pages/admin/genre/genre.component.ts
+++ manga-up-front/src/app/pages/admin/genre/genre.component.ts
@@ -1,12 +1,96 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { GenreService } from '../../../service/genre.service';
+import { GenreWithMangas } from '../../../type';
 
 @Component({
   selector: 'app-genre',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   standalone: true,
   templateUrl: './genre.component.html',
   styleUrl: './genre.component.scss'
 })
-export class GenreComponent {
+export class GenreAdminComponent implements OnInit {
+  idOfUrl!: number;
+  genreForm!: FormGroup;
+  genre: GenreWithMangas | null = null;
+
+    constructor(
+      private activatedRoute: ActivatedRoute,
+      private router: Router,
+      private genreService: GenreService,
+      private fb: FormBuilder
+    ) { }
+  ngOnInit(): void {
+    this.initForm();
+
+
+
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.idOfUrl = parseInt(id);
+      if (!isNaN(this.idOfUrl)) {
+        this.loadGenre(this.idOfUrl);
+      }
+    }
+
+    this.genreService.curentGenreSolo.subscribe(data => {
+      this.genre = data;
+      if (this.genre) {
+        // On met à jour le formulaire avec les données existantes
+        this.genreForm.patchValue({
+          label: this.genre.label,
+          description: this.genre.description,
+          url: this.genre.url
+        });
+      }
+      console.log('genre', this.genre);
+    });
+
+    
+  }
+
+
+
+  initForm(): void {
+    this.genreForm = this.fb.group({
+      label: ['', Validators.required],
+      description: ['', Validators.required],
+      url: ['', Validators.required]
+    });
+  }
+
+  loadGenre(id: number): void {
+    this.genreService.getGenreManga(id);
+  }
+
+
+
+  async onSubmit() {
+    if (this.genreForm.valid) {
+      const updatedGenre = {
+        id: this.idOfUrl,
+        ...this.genreForm.value
+      };
+
+      try {
+        console.log(updatedGenre);
+
+
+
+        await this.genreService.updateGenre(updatedGenre);
+        alert('Genre mis à jour avec succès !');
+        this.router.navigate(['/admin/categoriesAdmin']); // ou une autre route de retour
+      } catch (error) {
+        console.error('Erreur lors de la mise à jour', error);
+        alert('Erreur lors de la mise à jour');
+      }
+    } else {
+      this.genreForm.markAllAsTouched();
+    }
+  }
+
 
 }
