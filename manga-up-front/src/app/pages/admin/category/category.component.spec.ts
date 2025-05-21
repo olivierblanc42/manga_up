9067fce6b4250a4d42754c5d47333c0839d133e1
unknown <olivierhumanbooster@gmail.com>
2025-05-21 18:40:28 +0200

    ajout provideHttpClientTesting

--- manga-up-front/src/app/pages/admin/category/category.component.spec.ts
+++ manga-up-front/src/app/pages/admin/category/category.component.spec.ts
@@ -1,18 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CategoryComponent } from './category.component';
+import { CategoryAdminComponent } from './category.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('CategoryComponent', () => {
-  let component: CategoryComponent;
-  let fixture: ComponentFixture<CategoryComponent>;
+describe('CategoryAdminComponent', () => {
+  let component: CategoryAdminComponent;
+  let fixture: ComponentFixture<CategoryAdminComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CategoryComponent]
-    })
-    .compileComponents();
+      imports: [CategoryAdminComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '123', 
+              },
+            },
+            params: of({ id: '123' }), 
+          },
+        }
+      ]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(CategoryComponent);
+    fixture = TestBed.createComponent(CategoryAdminComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
