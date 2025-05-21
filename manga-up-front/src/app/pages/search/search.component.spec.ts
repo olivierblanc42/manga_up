313211c1055495af40d75be5415be33ac0d4b103
unknown <olivierhumanbooster@gmail.com>
2025-05-21 18:25:09 +0200

    mise en place des test

--- manga-up-front/src/app/pages/search/search.component.spec.ts
+++ manga-up-front/src/app/pages/search/search.component.spec.ts
@@ -1,16 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, of } from 'rxjs';
+import { SearchService } from './../../service/search.service';
+import { MangaBaseProjections } from '../../type';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
+  // Création d'un mock de SearchService
+  let mangasSearchSubject: BehaviorSubject<MangaBaseProjections | null>;
+  let searchServiceMock: {
+    currentmangasSearch: BehaviorSubject<MangaBaseProjections | null>,
+    getMangas: jasmine.Spy
+  };
+
   beforeEach(async () => {
+    mangasSearchSubject = new BehaviorSubject<MangaBaseProjections | null>(null);
+
+    searchServiceMock = {
+      currentmangasSearch: mangasSearchSubject,
+      getMangas: jasmine.createSpy('getMangas').and.returnValue(Promise.resolve())
+    };
+
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
-    })
-    .compileComponents();
+      imports: [SearchComponent],
+      providers: [
+        provideHttpClientTesting(),
+        { provide: SearchService, useValue: searchServiceMock },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({
+              get: (key: string) => 'a',
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
@@ -20,4 +49,10 @@ describe('SearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call getMangas on init', () => {
+    expect(searchServiceMock.getMangas).toHaveBeenCalledWith('a', 0);
+  });
+
+ 
 });
