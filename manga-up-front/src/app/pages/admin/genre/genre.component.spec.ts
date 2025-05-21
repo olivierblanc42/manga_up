39f6881aa713a2c14bfeee4d71e00e331136189b
unknown <olivierhumanbooster@gmail.com>
2025-05-21 18:42:09 +0200

    ajout provideHttpClientTesting

--- manga-up-front/src/app/pages/admin/genre/genre.component.spec.ts
+++ manga-up-front/src/app/pages/admin/genre/genre.component.spec.ts
@@ -1,18 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GenreComponent } from './genre.component';
+import { GenreAdminComponent } from './genre.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('GenreComponent', () => {
-  let component: GenreComponent;
-  let fixture: ComponentFixture<GenreComponent>;
+describe('GenreAdminComponent', () => {
+  let component: GenreAdminComponent;
+  let fixture: ComponentFixture<GenreAdminComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GenreComponent]
+      imports: [GenreAdminComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(), {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '123',
+              },
+            },
+            params: of({ id: '123' }),
+          },
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
-    fixture = TestBed.createComponent(GenreComponent);
+    fixture = TestBed.createComponent(GenreAdminComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
