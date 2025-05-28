c1831292c163bf58ce9bc805e6bffe24066f2ff8
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-05-28 18:02:39 +0200

    Test pour GenreUserService

--- manga-up-back/src/test/java/manga_up/manga_up/service/GenreUserServiceTest.java
+++ manga-up-back/src/test/java/manga_up/manga_up/service/GenreUserServiceTest.java
@@ -1,165 +1,159 @@
-// package manga_up.manga_up.service;
+package manga_up.manga_up.service;
 
-// import manga_up.manga_up.dao.GenderUserDao;
-// import manga_up.manga_up.dto.genderUser.GenderUserDto;
-// import manga_up.manga_up.mapper.CategoryMapper;
-// import manga_up.manga_up.mapper.GenderUserMapper;
-// import manga_up.manga_up.model.Category;
-// import manga_up.manga_up.model.GenderUser;
-// import manga_up.manga_up.projection.appUser.AppUserLittleProjection;
-// import manga_up.manga_up.projection.author.AuthorProjection;
-// import manga_up.manga_up.projection.genderUser.GenderUserProjection;
-// import manga_up.manga_up.projection.manga.MangaLittleProjection;
+import manga_up.manga_up.dao.GenderUserDao;
+import manga_up.manga_up.dto.genderUser.GenderUserDto;
+import manga_up.manga_up.mapper.CategoryMapper;
+import manga_up.manga_up.mapper.GenderUserMapper;
+import manga_up.manga_up.model.Category;
+import manga_up.manga_up.model.GenderUser;
+import manga_up.manga_up.projection.appUser.AppUserLittleProjection;
+import manga_up.manga_up.projection.author.AuthorProjection;
+import manga_up.manga_up.projection.genderUser.GenderUserProjection;
+import manga_up.manga_up.projection.manga.MangaLittleProjection;
 
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.data.domain.Page;
-// import org.springframework.data.domain.PageImpl;
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.data.domain.Pageable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-// import java.time.LocalDate;
-// import java.util.Arrays;
-// import java.util.List;
-// import java.util.Optional;
-// import java.util.Set;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
-// import static org.assertj.core.api.Assertions.assertThat;
-// import static org.mockito.ArgumentMatchers.any;
-// import static org.mockito.Mockito.verify;
-// import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-// @SpringBootTest
-// class GenreUserServiceTest {
-// @Mock
-// private GenderUserDao genderUserDao;
-// @Mock
-// private GenderUserMapper genderUserMapper;
-// @InjectMocks
-// private GenreUserService genreUserService;
+@ExtendWith(MockitoExtension.class)
+class GenreUserServiceTest {
+    @Mock
+    private GenderUserDao genderUserDao;
+    @Mock
+    private GenderUserMapper genderUserMapper;
+    @InjectMocks
+    private GenreUserService genreUserService;
 
+    private static class TestGenderUserProjection implements GenderUserProjection {
+        private final Integer id;
+        private final String label;
+        private final Set<AppUserLittleProjection> appUsers;
 
-//     private static class TestGenderUserProjection implements GenderUserProjection {
-//         private final  Integer id;
-//         private final String label;
-//         private final Set<AppUserLittleProjection> appUsers;
+        private TestGenderUserProjection(Integer id, String label, Set<AppUserLittleProjection> appUsers) {
+            this.id = id;
+            this.label = label;
+            this.appUsers = appUsers;
+        }
 
-//         private TestGenderUserProjection(Integer id, String label, Set<AppUserLittleProjection> appUsers) {
-//             this.id = id;
-//             this.label = label;
-//             this.appUsers = appUsers;
-//         }
+        @Override
+        public Integer getId() {
+            return id;
+        }
 
-//         @Override
-//         public Integer getId() {
-//             return id;
-//         }
+        @Override
+        public String getLabel() {
+            return label;
+        }
 
-//         @Override
-//         public String getLabel() {
-//             return label;
-//         }
+        @Override
+        public Set<AppUserLittleProjection> getAppUsers() {
+            return appUsers;
+        }
+    }
 
-//         @Override
-//         public Set<AppUserLittleProjection> getAppUsers() {
-//             return appUsers;
-//         }
-//     }
+    @Test
+    void shouldReturnAllGenreUsers() {
+        Pageable pageable = PageRequest.of(0, 5);
 
+        GenderUserProjection gender1 = new TestGenderUserProjection(
+                1,
+                "Man",
+                Set.of());
+        GenderUserProjection gender2 = new TestGenderUserProjection(
+                1,
+                "Woman",
+                Set.of());
 
+        Page<GenderUserProjection> page = new PageImpl<>(List.of(gender1, gender2));
+        when(genderUserDao.getGenderUser(pageable)).thenReturn(page);
 
-//     @Test
-//     void shouldReturnAllGenreUsers() {
-//         Pageable pageable = PageRequest.of(0, 5);
+        Page<GenderUserProjection> result = genreUserService.getGenreUsers(pageable);
 
-//         GenderUserProjection gender1 = new TestGenderUserProjection(
-//                 1,
-//                 "Man",
-//                 Set.of()
-//         );
-//         GenderUserProjection gender2 = new TestGenderUserProjection(
-//                 1,
-//                 "Woman",
-//                 Set.of()
-//         );
+        assertThat(result).hasSize(2).containsExactly(gender1, gender2);
+    }
 
-//         Page<GenderUserProjection> page = new PageImpl<>(List.of(gender1, gender2));
-//         when(genderUserDao.getGenderUser(pageable)).thenReturn(page);
+    @Test
+    void shouldReturnGenreUserById() {
 
-//         Page<GenderUserProjection> result = genreUserService.getGenreUsers(pageable);
+        GenderUserProjection gender = new TestGenderUserProjection(
+                1,
+                "Man",
+                Set.of());
 
-//         assertThat(result).hasSize(2).containsExactly(gender1, gender2);
-//     }
+        when(genderUserDao.findGenderUserProjectionById(1)).thenReturn(Optional.of(gender));
 
-//     @Test
-//     void shouldReturnGenreUserById() {
+        GenderUserProjection result = genreUserService.getGenreUserById(1);
+        assertThat(result).isEqualTo(gender);
 
-//         GenderUserProjection gender = new TestGenderUserProjection(
-//                 1,
-//                 "Man",
-//                 Set.of()
-//         );
+    }
 
-//         when(genderUserDao.findGenderUserProjectionById(1)).thenReturn(Optional.of(gender));
+    @Test
+    void deleteGenreUserById() {
+        GenderUser gender = new GenderUser();
+        gender.setId(1);
+        gender.setLabel("Man");
+        gender.setAppUsers(Set.of());
+        when(genderUserDao.findGenderById(1)).thenReturn(Optional.of(gender));
+        genreUserService.deleteGenreUserById(1);
+        verify(genderUserDao).delete(gender);
 
-//         GenderUserProjection result = genreUserService.getGenreUserById(1);
-//         assertThat(result).isEqualTo(gender);
+    }
 
-//     }
+    @Test
+    void saveGenreUser() {
+        GenderUserDto genderUserDto = new GenderUserDto(1, "Man");
+        GenderUser genderUserEntity = new GenderUser();
+        genderUserEntity.setId(genderUserDto.getId());
+        genderUserEntity.setLabel(genderUserDto.getLabel());
 
-//     @Test
-//     void deleteGenreUserById() {
-//         GenderUser gender = new GenderUser();
-//         gender.setId(1);
-//         gender.setLabel("Man");
-//         gender.setAppUsers(Set.of());
-//         when(genderUserDao.findGenderById(1)).thenReturn(Optional.of(gender));
-//         genreUserService.deleteGenreUserById(1);
-//         verify(genderUserDao).delete(gender);
+        // Appelle les méthodes sur le MOCK, pas la classe
+        when(genderUserMapper.toEntity(genderUserDto)).thenReturn(genderUserEntity);
+        when(genderUserDao.save(genderUserEntity)).thenReturn(genderUserEntity);
+        when(genderUserMapper.toDto(genderUserEntity)).thenReturn(genderUserDto);
 
-//     }
+        GenderUserDto result = genreUserService.saveGenreUser(genderUserDto);
 
-//     @Test
-//     void saveGenreUser() {
-//         int id = 1;
-//         GenderUserDto genderDto = new GenderUserDto(
-//                 "Man"
-//         );
-//         GenderUser genderEntity = new GenderUser();
-//         genderEntity.setId(id);
-//         genderEntity.setLabel("Man");
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(genderUserDto);
+    }    
 
-//         when(genderUserMapper.toEntity(genderDto)).thenReturn(genderEntity);
-//         when(genderUserDao.save(genderEntity)).thenReturn(genderEntity);
-//         when(genderUserMapper.toDto(genderEntity)).thenReturn(genderDto);
+    @Test
+    void ShouldUpdateGenreUser() {
+        int id = 1;
+        GenderUserDto genderDto = new GenderUserDto(1, "Man3");
+        GenderUser genderEntity = new GenderUser();
+        genderEntity.setId(id);
+        genderEntity.setLabel("Man");
 
-//         GenderUserDto result = genreUserService.saveGenreUser(genderDto);
+        when(genderUserDao.findGenderById(id)).thenReturn(Optional.of(genderEntity));
+        when(genderUserDao.save(genderEntity)).thenReturn(genderEntity);
+        when(genderUserMapper.toDto(genderEntity)).thenReturn(genderDto); // <<< ligne manquante
 
-//         assertThat(result).isNotNull();
-//         assertThat(result).isEqualTo(genderDto);
-//     }
+        GenderUserDto result = genreUserService.updateGenreUser(id, genderDto);
 
-//     @Test
-//     void ShouldUpdateGenreUser() {
-//         int id = 1;
-//         GenderUserDto genderDto = new GenderUserDto(
-//                 "Man3"
-//         );
-//         GenderUser genderEntity = new GenderUser();
-//         genderEntity.setId(id);
-//         genderEntity.setLabel("Man");
-
-//         when(genderUserDao.findGenderById(1)).thenReturn(Optional.of(genderEntity));
-//         genderEntity.setLabel(genderDto.getLabel());
-//         when(genderUserDao.save(genderEntity)).thenReturn(genderEntity);
-//         when(genderUserMapper.toDto(genderEntity)).thenReturn(genderDto);
-
-//         GenderUserDto result = genreUserService.updateGenreUser(id ,genderDto);
-//         assertThat(result).isNotNull();
-//         assertThat(result).isEqualTo(genderDto);
-//         assertThat(result.getLabel()).isEqualTo("Man3");
-//     }
-// }
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(genderDto);
+        assertThat(result.getLabel()).isEqualTo("Man3");
+    }
+    
+}
