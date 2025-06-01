fd9a95208aee40318fb5a591cf2ff47e9c53639c
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-06-01 08:29:01 +0200

    openApiConfig

---
+++ manga-up-back/src/main/java/manga_up/manga_up/configuration/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package manga_up.manga_up.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("https://mangaup-production.up.railway.app")));
+    }
+}
