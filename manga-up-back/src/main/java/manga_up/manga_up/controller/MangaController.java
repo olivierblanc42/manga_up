7368c5aa37f2d3309b58f8dfce99f6dd5afeaadb
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-06-03 14:44:56 +0200

    netoyage du fichier et modification methode delete avec ajout PathVariable

--- manga-up-back/src/main/java/manga_up/manga_up/controller/MangaController.java
+++ manga-up-back/src/main/java/manga_up/manga_up/controller/MangaController.java
@@ -3,10 +3,8 @@ package manga_up.manga_up.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import manga_up.manga_up.dto.manga.MangaDto;
-import manga_up.manga_up.dto.manga.MangaDtoOne;
 import manga_up.manga_up.dto.manga.MangaDtoRandom;
 import manga_up.manga_up.projection.manga.MangaBaseProjection;
 import manga_up.manga_up.projection.manga.MangaProjection;
@@ -57,22 +55,6 @@ public class MangaController {
         return ResponseEntity.ok(mangaService.findMangaById(id));
     }
 
-    // @Operation(summary ="Get Random Four Mangas")
-    // @GetMapping("/four")
-    // public ResponseEntity<List<MangaDtoRandom>> getRandomFourMangas(){
-    // LOGGER.info("Get six Mangas");
-    // List<MangaDtoRandom> mangas = mangaService.getReleaseDateRaw();
-    // LOGGER.info("Found {} mangas", mangas.size());
-    // return new ResponseEntity<>(mangas, HttpStatus.OK);
-    // }
-
-    // @Operation(summary ="Get Random Manga")
-    // @GetMapping("/one")
-    // public ResponseEntity<List<MangaDtoOne>> getRandomManga(){
-    // LOGGER.info("Get Manga");
-    // List<MangaDtoOne> mangas = mangaService.getRandomManga();
-    // return new ResponseEntity<>(mangas, HttpStatus.OK);
-    // }
     @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Adding Manga")
     @PostMapping("/add")
@@ -84,34 +66,12 @@ public class MangaController {
     @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "delete manga")
     @DeleteMapping("/{id}")
-    public void deleteManga(Integer mangaId) {
-        LOGGER.info("Deleting manga by id");
+    public void deleteManga(@PathVariable("id") Integer mangaId) {
+        LOGGER.info("Deleting manga by id " + mangaId);
         mangaService.deleteManga(mangaId);
-    }
+    }    
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
-    public ResponseEntity<MangaDto> updateManga(@PathVariable Integer id, @RequestBody MangaDto mangaUpdateDto) {
-        try {
-            MangaDto updateManga = mangaService.updateManga(id, mangaUpdateDto);
-            return ResponseEntity.ok(updateManga);
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
 
-    // @Operation(summary = "All Mangas with pagination")
-    // @ApiResponse(responseCode = "201", description = "All manga have been
-    // retrieved")
-    // @GetMapping("paginations")
-    // public ResponseEntity<Page<MangaBaseProjection>> getAllMangaPicture(
-    // @PageableDefault(page = 0, size = 8, sort = "id", direction =
-    // Sort.Direction.DESC) @ParameterObject Pageable pageable) {
-    // LOGGER.info("Find all addresses with pagination");
-    // Page<MangaBaseProjection> mangas = mangaService.getTest(pageable);
-    // LOGGER.info("Found {} addresses", mangas.getTotalElements());
-    // return new ResponseEntity<>(mangas, HttpStatus.OK);
-    // }
     @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "All Mangas with pagination")
     @ApiResponse(responseCode = "201", description = "All manga have been retrieved")
@@ -120,5 +80,4 @@ public class MangaController {
         List<MangaDtoRandom> mangas = mangaService.getFourMangaRandom();
         return new ResponseEntity<>(mangas, HttpStatus.OK);
     }
-
 }
