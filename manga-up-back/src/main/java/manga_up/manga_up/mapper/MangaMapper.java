133b831bb3574c92f3d1c736f7b49ef44e0f72cf
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-06-03 15:11:26 +0200

    netoyyage du fichier

--- manga-up-back/src/main/java/manga_up/manga_up/mapper/MangaMapper.java
+++ manga-up-back/src/main/java/manga_up/manga_up/mapper/MangaMapper.java
@@ -1,35 +1,56 @@
 package manga_up.manga_up.mapper;
 
+import manga_up.manga_up.dao.GenreDao;
 import manga_up.manga_up.dto.manga.MangaDto;
 import manga_up.manga_up.dto.manga.MangaLightDto;
 import manga_up.manga_up.model.*;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
 public class MangaMapper {
 
- private final   CategoryMapper categoryMapper;
-    private final   GenderMangaMapper genderMapper;
-    private final   AuthorMapper authorMapper;
-    private final   PictureMapper pictureMapper;
-    private final   FavoriteMapper appUserMapper;
+    private final CategoryMapper categoryMapper;
+    private final GenderMangaMapper genderMapper;
+    private final AuthorMapper authorMapper;
+    private final PictureMapper pictureMapper;
+    private final FavoriteMapper appUserMapper;
+    private final GenreDao genreDao;
 
-    public MangaMapper(CategoryMapper categoryMapper, GenderMangaMapper genderMapper, AuthorMapper authorMapper, PictureMapper pictureMapper, 
-            FavoriteMapper appUserMapper) {
+    public MangaMapper(CategoryMapper categoryMapper, GenderMangaMapper genderMapper, AuthorMapper authorMapper,
+            PictureMapper pictureMapper,
+            FavoriteMapper appUserMapper, GenreDao genreDao) {
         this.categoryMapper = categoryMapper;
         this.genderMapper = genderMapper;
         this.authorMapper = authorMapper;
         this.pictureMapper = pictureMapper;
         this.appUserMapper = appUserMapper;
+        this.genreDao = genreDao;
     }
 
-
     public MangaDto mangaToMangaDto(Manga manga) {
-       return new MangaDto(
+
+        Set<Integer> authorsIds = Collections.emptySet();
+        if (manga.getAuthors() != null) {
+            authorsIds = manga.getAuthors()
+                    .stream()
+                    .map(Author::getId)
+                    .collect(Collectors.toSet());
+        }
+
+        Set<Integer> genresIds = Collections.emptySet();
+        if (manga.getGenres() != null) {
+            genresIds = manga.getGenres()
+                    .stream()
+                    .map(Genre::getId)
+                    .collect(Collectors.toSet());
+        }
+
+        return new MangaDto(
                 manga.getTitle(),
                 manga.getSubtitle(),
                 manga.getReleaseDate(),
@@ -39,15 +60,15 @@ public class MangaMapper {
                 manga.getInStock(),
                 manga.getActive(),
                 categoryMapper.toLittleDtoCategory(manga.getIdCategories()),
-                genderMapper.toLightDtoGenres(manga.getGenres()),
-                authorMapper.toLigthDtoAuthorSet(manga.getAuthors()),
+                genresIds,
+                authorsIds,
                 pictureMapper.toPictureLightDtoSet(manga.getPictures()),
                 appUserMapper.toUserFavoriteDtoSet(manga.getAppUsers())
 
         );
     }
 
- public Manga mangaToEntity(MangaDto mangaDto) {
+    public Manga mangaToEntity(MangaDto mangaDto) {
         Manga manga = new Manga();
         manga.setTitle(mangaDto.getTitle());
         manga.setSubtitle(mangaDto.getSubtitle());
@@ -59,11 +80,12 @@ public class MangaMapper {
         manga.setActive(mangaDto.getActive());
         manga.setIdCategories(categoryMapper.categoryLittleDto(mangaDto.getIdCategories()));
 
-
-        Set<Genre> genres = genderMapper.toEntityGenres(mangaDto.getGenres());
+        Set<Genre> genres = mangaDto.getGenres().stream()
+                .map(genreId -> genreDao.findById(genreId)
+                        .orElseThrow(() -> new RuntimeException("Genre not found: " + genreId)))
+                .collect(Collectors.toSet());
         manga.setGenres(genres);
-        Set<Author> authors = authorMapper.toEntityAuthors(mangaDto.getAuthors());
-        manga.setAuthors(authors);
+
         Set<Picture> pictures = pictureMapper.toEntityPictures(mangaDto.getPictures());
         manga.setPictures(pictures);
 
@@ -71,30 +93,31 @@ public class MangaMapper {
         manga.setAppUsers(appUsers);
 
         return manga;
- }
+    }
 
- public MangaLightDto toMangaLightDto(Manga manga) {
-     return new MangaLightDto(
-             manga.getId(),
-             manga.getTitle());
- }
- public Set<MangaLightDto> toMangaLightDtoSet(Set<Manga> mangas) {
-     return mangas.stream()
-             .map(this::toMangaLightDto)
-             .collect(Collectors.toSet());
- }
+    public MangaLightDto toMangaLightDto(Manga manga) {
+        return new MangaLightDto(
+                manga.getId(),
+                manga.getTitle());
+    }
 
-public Manga toEntityManga(MangaLightDto mangaLightDto) {
-     Manga manga = new Manga();
-     manga.setId(mangaLightDto.getId());
-     manga.setTitle(mangaLightDto.getTitle());
-     return manga;
- }
+    public Set<MangaLightDto> toMangaLightDtoSet(Set<Manga> mangas) {
+        return mangas.stream()
+                .map(this::toMangaLightDto)
+                .collect(Collectors.toSet());
+    }
 
- public Set<Manga> toEntityMangas(Set<MangaLightDto> mangas) {
-     return mangas.stream()
-             .map(this::toEntityManga)
-             .collect(Collectors.toSet());
- }
- 
+    public Manga toEntityManga(MangaLightDto mangaLightDto) {
+        Manga manga = new Manga();
+        manga.setId(mangaLightDto.getId());
+        manga.setTitle(mangaLightDto.getTitle());
+        return manga;
+    }
+
+    public Set<Manga> toEntityMangas(Set<MangaLightDto> mangas) {
+        return mangas.stream()
+                .map(this::toEntityManga)
+                .collect(Collectors.toSet());
+    }
+
 }
