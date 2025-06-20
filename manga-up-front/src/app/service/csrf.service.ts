1105a22ba3ab927d0d901694ae9eb5ff9eef4e9d
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-06-20 14:46:07 +0200

    Service csrf

---
+++ manga-up-front/src/app/service/csrf.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, lastValueFrom } from 'rxjs';
+import { Csrf } from '../type';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CsrfService {
+    private apiUrl = `/api/csrf`;
+
+    csrfProjections = new BehaviorSubject<Csrf | null>(null);
+    currentCsrfProjection = this.csrfProjections.asObservable();
+
+    constructor(private http: HttpClient) { }
+
+    async fetchCsrfToken(): Promise<void> {
+        try {
+            const response = await lastValueFrom(this.http.get<Csrf>(this.apiUrl));
+            if (!response) return;
+            this.csrfProjections.next(response);
+        } catch (error) {
+            console.error('Erreur lors de la récupération du token CSRF:', error);
+        }
+    }
+
+    getToken(): string | null {
+        const csrf = this.csrfProjections.value;
+        return csrf ? csrf.csrfToken : null;
+    }
+      
+
+
+}
