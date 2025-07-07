587195a643ef786ef438673b3f4e3e2a1d1b9902
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-07-07 16:54:05 +0200

    bug fix cor origin

--- manga-up-front/src/main.ts
+++ manga-up-front/src/main.ts
@@ -1,16 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+
 import { AppComponent } from './app/app.component';
 import { authInterceptor } from './app/auth.interceptor';
 import { routes } from './app/app.routes';
+
 import { register } from 'swiper/element/bundle';
 register();
+
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), 
-    provideHttpClient(withInterceptors([authInterceptor])), 
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN'
+      })
+    ),
   ],
-}).catch((err) => console.error(err));
-
-
+}).catch(err => console.error(err));
