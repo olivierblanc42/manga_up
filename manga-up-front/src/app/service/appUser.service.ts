b5bb6ee8b60430a12cf7040b412844ad30216887
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-07-11 14:23:46 +0200

    add update user

--- manga-up-front/src/app/service/appUser.service.ts
+++ manga-up-front/src/app/service/appUser.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
-import { UserTest } from '../type';
-import { environment } from '../../environments/environment.prod';
+import { UserTest, UserUpdate } from '../type';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
@@ -10,7 +10,8 @@ import { environment } from '../../environments/environment.prod';
 export class AppUserService {
 
     private apiUrl = `api/users/me`;
-    
+    private apiUpdate = `api/users/update/me`;
+
 
     options = {
         headers: new HttpHeaders({
@@ -28,11 +29,15 @@ export class AppUserService {
     userMe = new BehaviorSubject<UserTest | null>(null);
     currentUserMe = this.userMe.asObservable();
 
+    userMeUpdate = new BehaviorSubject<UserUpdate | null>(null);
+    currentUserMeUpdate = this.userMeUpdate.asObservable();
+
+
+
+
     async loadCurrentUser() {
         try {
-            const user = await firstValueFrom(
-                this.http.get<UserTest>(this.apiUrl, { withCredentials: true })
-            );
+            const user = await firstValueFrom(this.http.get<UserTest>(this.apiUrl));
             if (user) {
                 console.log('Utilisateur chargé :', user);
                 this.userMe.next(user);
@@ -41,7 +46,22 @@ export class AppUserService {
             console.error('Erreur lors du chargement de l\'utilisateur :', error);
         }
     }
-    
+
+
+
+    async updateCurrentUser(currentUser: UserUpdate): Promise<UserUpdate> {
+        try {
+            const urlWithId = `${this.apiUpdate}`;
+            const updatedUser = await firstValueFrom(
+                this.http.put<UserUpdate>(urlWithId, currentUser, { withCredentials: true })
+            );
+            this.userMeUpdate.next(updatedUser);
+            return updatedUser;
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour:', error);
+            throw error;
+        }
+    }
 
 }
 
